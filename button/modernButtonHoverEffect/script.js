f9b15b0423453c1107b3ff5b778fb237a58c40cd
Sergey Kurganov <pi_2002@mail.ru>
2024-12-19 15:30:49 +0600

    add Modern Button Hover Effects

---
+++ button/modernButtonHoverEffect/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let btns = document.querySelectorAll('.btns')
+  btns.forEach(function(btn) {
+    const spans =[]
+    for (let i = 0; i < 40; i++) {
+      let span = document.createElement('span')
+      span.style.top = `${i * 2}px`
+      spans.push(span)
+      btn.appendChild(span)
+
+      let randomDelay = (Math.random() * 0.75) + 0
+      span.style.transitionDelay = randomDelay + 's'
+    }
+  });
+})
